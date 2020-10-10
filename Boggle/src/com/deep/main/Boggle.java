59462c6b1a0522e140d6e9b7a29cfaed38ca50dd
Deep Shah <deepshah303@gmail.com>
2020-10-10 15:40:27 +0530

    Boggle first commit

---
+++ Boggle/src/com/deep/main/Boggle.java
@@ -0,0 +1,19 @@
+package com.deep.main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Boggle {
+
+	
+	static String[] strArray = { "apple", "pickle", "side", "kick", "sick", "mood", "cat", "cats", "man", "super",
+			"antman", "godzilla", "dog", "dot", "sine", "cos", "signal", "bitcoin", "cool", "zapper" };
+	
+	public static List<String> englishWords = Arrays.asList(strArray);
+
+	public static void main(String[] args) {
+
+	}
+
+}
